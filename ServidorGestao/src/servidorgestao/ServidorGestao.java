467c6cf1e4492846306af2681961a6ae955193ca
tiago719 <tiagoaacoutinho@gmail.com>
2017-11-20 18:10:05 +0000

    primeiro commit

---
+++ ServidorGestao/src/servidorgestao/ServidorGestao.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servidorgestao;
+
+/**
+ *
+ * @author Tiago Coutinho
+ */
+public class ServidorGestao
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        //comit
+        // TODO code application logic here
+    }
+    
+}
