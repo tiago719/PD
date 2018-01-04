8a3189b6c83245d1e52a6d585dd162a01302e169
andre <andre@DESKTOP-G42UQS2>
2018-01-04 21:19:24 +0000

    Comunicaçao ServidorGESTAO - ServidorJogo

---
+++ ServidorJogo/src/Model/TrataServidorGestao.java
@@ -0,0 +1,80 @@
+package Model;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class TrataServidorGestao extends Thread {
+
+    public String IpDB = null, IpSG;
+    public int PortoSG;
+    public static final int MAX_SIZE = 10000;
+
+    @Override
+    public void run() {
+
+        InetAddress serverAddr = null;
+        try {
+            serverAddr = InetAddress.getByName(IpSG);
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(TrataServidorGestao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        int serverPort = PortoSG;
+        DatagramSocket socket = null;
+        ByteArrayOutputStream bOut;
+        ObjectOutputStream out;
+        DatagramPacket packet;
+        ObjectInputStream in;
+        String request;
+
+        try {
+            socket = new DatagramSocket();
+        } catch (SocketException ex) {
+            Logger.getLogger(TrataServidorGestao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        while (true) {
+            try {
+                //ENVIA HEARTBEAT
+                bOut = new ByteArrayOutputStream();
+
+                out = new ObjectOutputStream(bOut);
+
+                out.writeObject(new Boolean(true));
+                out.flush();
+
+                packet = new DatagramPacket(bOut.toByteArray(), bOut.size(),
+                        serverAddr, serverPort);
+                socket.send(packet);
+
+                //RECEBE IP 
+                packet = new DatagramPacket(new byte[MAX_SIZE], MAX_SIZE);
+                socket.receive(packet);
+
+                in = new ObjectInputStream(new ByteArrayInputStream(packet.getData(), 0, packet.getLength()));
+
+                try {
+                    request = (String) (in.readObject());
+                    IpDB = request;
+                } catch (ClassCastException | ClassNotFoundException e) {
+                    request = null;
+                }
+
+                if (request == null) {
+                    Thread.currentThread().stop();
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(TrataServidorGestao.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+    }
+}
--- ServidorJogo/src/servidorjogo/ServidorJogo.java
+++ ServidorJogo/src/servidorjogo/ServidorJogo.java
@@ -1,13 +1,23 @@
 
 package servidorjogo;
 
+import Model.TrataServidorGestao;
+
+
 ///ARAY LIST COM MODELOS DE JOGO
 
 public class ServidorJogo {
 
    
     public static void main(String[] args) {
-        
+       TrataServidorGestao thread1 = new TrataServidorGestao();
+       thread1.IpDB = "localhost";
+       thread1.IpSG = "localhost";
+       thread1.PortoSG = 6005;
+       thread1.start();
+       
+       
+       
     }
     
 }
