592a2a3c51311e60c2d214c53d3fe175bbddfa20
EduFernandes97 <edu.cadete.fernandes@gmail.com>
2018-01-13 19:41:31 +0000

    tentar corrigir jogadas

--- ServidorJogo/src/Model/TrataUtilizador.java
+++ ServidorJogo/src/Model/TrataUtilizador.java
@@ -33,83 +33,8 @@ public class TrataUtilizador extends Thread {
             server = new ServerSocket(ConstantesIps.PORTOSERVIDORJOGO);
             while (true) {
                 Socket nextCliente = server.accept();
-
-                ObjectOutputStream out = new ObjectOutputStream(nextCliente.getOutputStream());
-                out.flush();
-                ObjectInputStream in = new ObjectInputStream(nextCliente.getInputStream());
-
-                Object objectRecebidoUtilizador = in.readObject();
-
-                if (objectRecebidoUtilizador instanceof AcoesPartida) 
-                {
-                    AcoesPartida ap = (AcoesPartida) objectRecebidoUtilizador;
-
-                    ResultSet rs;
-
-                    rs = BD.Le("select count(*) as 'hasPar' from par where idPar = " + ap.getIdPar());
-                    rs.next();
-                    if (rs.getInt("hasPar") < 1) {
-                        continue;
-                    }
-
-                    switch (ap.getAcao()) {
-                        case 1:
-                            rs = BD.Le("select count(*) as 'jogoCriado' from jogo where emcurso = 1 and idpar = " + ap.getIdPar());
-                            rs.next();
-                            if (rs.getInt("jogoCriado") != 0){
-                                out.writeObject(jogosDecorrer.getGameModel(ap.getIdPar()));
-                                out.flush();
-                                break;
-                            }
-                            
-                            rs = BD.Modifica("INSERT INTO jogo (IDJOGO, "
-                                    + "IDPAR, RESULTADO, VENCEDOR,"
-                                    + " EMCURSO, TERMINOU, INTERROMPIDO) "
-                                    + "VALUES (NULL, '" + ap.getIdPar()
-                                    + "', '-1', '-1', '1', '0', '0')");
-                            rs.next();
-
-                            int idJogo = rs.getInt(1);
-                            if (idJogo > 0) {
-
-                                int idPar = ap.getIdPar();
-
-                                rs = BD.Le("SELECT IDU1, IDU2 FROM `par`"
-                                        + " WHERE `IDPAR` = " + idPar);
-                                rs.next();
-
-                                int idU1 = rs.getInt("IDU1");
-                                int idU2 = rs.getInt("IDU2");
-
-                                rs = BD.Le("SELECT USERNAME FROM `utilizador`"
-                                        + " WHERE IDUTILIZADOR = " + idU1);
-                                rs.next();
-                                String nick1 = rs.getString("USERNAME");
-                                rs = BD.Le("SELECT USERNAME FROM `utilizador` "
-                                        + "WHERE IDUTILIZADOR = " + idU2);
-                                rs.next();
-                                String nick2 = rs.getString("USERNAME");
-
-                                rs.next();
-                                jogosDecorrer.addNovoJogo(idJogo, nick1, nick2, ap.getIdPar());
-                                //TODO: Se existir ficheiro de modelo jogo usar esse e nao fazer novo gamemodel
-                                out.writeObject(jogosDecorrer.getGameModel(idPar));
-                                out.flush();
-                            }
-                            break;
-                        case 2:
-                            //TODO: CONCLUIR JOGO
-
-                            break;
-
-                        case 3:
-                            //TODO: CANCELAR JOGO
-
-                            break;
-
-                    }
-                }
-
+                Thread t1 = new RecebeAcoes(nextCliente, jogosDecorrer,BD );
+                t1.start();
             }
         } catch (Exception ex) {
 
